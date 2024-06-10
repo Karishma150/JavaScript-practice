532b3dfc9d1fa5c669d68bb27ab7b27b6e906734
Karishma150 <86237235+Karishma150@users.noreply.github.com>
2024-06-10 15:48:22 +0000

    config dataType done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version of JS
+
+//alert(4+6) // we are using nodejs not browser
+
+//console.log(2+3) console.log("kannu"); // error coz seicolon is expected here in between 
+                                       // Also the code readability is not high
+
+//tc39.es,MDN  to read about JS
+
+let name="Karishma" //string
+let age=22 //number
+let isLoggedIn = true //boolean
+
+//PRIMITIVE DATA TYPES
+//number=> range 2 to the power 53
+// bigInt => used for large no.s like in stock markets,trading or big websites.
+//string => ""
+//boolean => true/false
+//null => standalone value(it is also a type) . Null is a representation of empty value.
+          // let temperature = null
+
+//undefined => no value is assigned
+//symbol => it is used for uniqueness,for individual components for ex: in figma tools.
+
+//NON PRIMITIVE DATA TYPE
+
+//object
+
+console.log(typeof 23)
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
